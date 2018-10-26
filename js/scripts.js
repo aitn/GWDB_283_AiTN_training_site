7a3cf11599abd5ac0e04e48699bdef9534b5fbb4
Joel Norris <joel.r.norris@gmail.com>
2018-10-25 19:43:24 -0500

    added eyeCon and js to change the icon and password field type

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 
+$(document).ready(function(){
+
+  let $passwordInput = $("#passwordInput");
+  let $showHidePassword = $("#showHidePassword");
+  let $eyeCon = $("#eyeCon");
+
+  console.log(showHidePassword);
+
+  $showHidePassword.on('click', function(event){
+    event.preventDefault();
+    console.log("eyeball was clicked");
+
+    if ($passwordInput.attr("type") == "password") {
+      console.log("type is password")
+      $eyeCon.removeClass("fa-eye-slash");
+      $eyeCon.addClass("fa-eye");
+      $passwordInput.attr("type", "text");
+    } else {
+      $eyeCon.removeClass("fa-eye");
+      $eyeCon.addClass("fa-eye-slash");
+      $passwordInput.attr("type", "password");
+    }
+  });
+
+});
